7c08d2146e3fbb84e4bf3350453db0bc94f219a4
Kushagra910 <kushagram63@gmail.com>
2023-07-14 04:19:49 +0530

    Added Course Schedule I

---
+++ Graphs/TopoSort&Problems/CourseSchedule1.cpp
@@ -0,0 +1,50 @@
+// 207. Course Schedule
+
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+
+//after analysing the question boils down to detect a cycle in a directed Graph
+// Can Apply BFS/DFS to solve it 
+// Kahn's algo is applied 
+ 
+// TC = O(n+E)
+// SC = O(n)
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int>adj[n];
+        for(auto it:prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj[v].push_back(u);
+        }
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        vector<int>topoSort;
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topoSort.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topoSort.size() == n) return true;
+        else return false;
+    }
+};
