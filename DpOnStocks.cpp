8fdd6802ab9cd4c1116984adfba7324b3f19fdf6
Kushagra910 <kushagram63@gmail.com>
2023-07-15 02:39:27 +0530

    Updated TC

--- DpOnStocks.cpp
+++ DpOnStocks.cpp
@@ -1,9 +1,12 @@
-
+// 121. Best Time to Buy and Sell Stock
 
 // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+// if you are selling a stock on ith day,you need to buy on the minimum price from the 1 to i-1th day .
+// TC = O(n)
+// SC = O(1)
  class Solution {
 public:
     int maxProfit(vector<int>& prices) {
